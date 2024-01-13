d61ad6f9f44de1d41e00e1879af620002c1f594f
IrekM <12935670+irekM@users.noreply.github.com>
2024-01-13 14:00:27 +0100

    cra, main styled components and header section

---
+++ react-rmdb/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//Components
+import Header from './components/Header';
+
+//styles
+import { GLobalStyle } from './GlobalStyle';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Start here.
+      <GLobalStyle />
+    </div>
+  );
+}
+
+export default App;
