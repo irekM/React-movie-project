beeb30c49c8de084ba9e0911674728b3393b95ff
IrekM <12935670+irekM@users.noreply.github.com>
2024-01-13 15:45:21 +0100

    fetch data from api for home component

--- react-rmdb/src/components/Home.js
+++ react-rmdb/src/components/Home.js
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+//Api
+import API from '../API'
 
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config'
@@ -15,6 +17,31 @@ const Home = () => {
     const[state, setState] = useState();
     const [loading, setLoading] = useState(false);
     const[error, setError] = useState(false);
+
+    const fetchMovies = async(page, searchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+            
+            setState(prev => ({
+                ...movies,
+                results: 
+                page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }))
+
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    };
+
+    //initial render
+    useEffect(() => {
+        fetchMovies(1)
+    },[])
+    console.log(state);
 
     return <div>Home Page</div>
 }
