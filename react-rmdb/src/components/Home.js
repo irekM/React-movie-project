429edc5dff143005b39964b3e2d8a80a57981154
IrekM <12935670+irekM@users.noreply.github.com>
2024-02-03 14:04:31 +0100

    add spinner loading logo for movie section

--- react-rmdb/src/components/Home.js
+++ react-rmdb/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 
 
 //config
@@ -8,6 +8,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
 import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
+import Spinner from "./Spinner";
 
 //Hooks
 import {useHomeFetch} from '../hooks/useHomeFetch';
@@ -43,6 +44,7 @@ const Home = () => {
         ))}
 
      </Grid>
+     <Spinner />
     </>
     );
 };
