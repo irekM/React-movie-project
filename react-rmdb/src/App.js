209606acfc254dedd7b3c70bc538f3808ba61a55
IrekM <12935670+irekM@users.noreply.github.com>
2024-02-10 14:33:57 +0100

    Try to introduce basics of React Routing

--- react-rmdb/src/App.js
+++ react-rmdb/src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
+//Routing
+import { BroserRouter as Router, Routes, Route} from 'react-router-dom';
 //Components
 import Header from './components/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
 //styles
 import { GLobalStyle } from './GlobalStyle';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => (
+    <Router>
       <Header />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/:movieId' element={<Movie />} />
+        <Route path='/*'  element={<NotFound />} />
+      </Routes>
       <Home />
       <GLobalStyle />
-    </div>
+    </Router>
   );
-}
+
 
 export default App;
