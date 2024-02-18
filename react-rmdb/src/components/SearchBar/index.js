32197c1069d19590aaf878b59c8d0d637c1be6cb
IrekM <12935670+irekM@users.noreply.github.com>
2024-02-18 16:09:32 +0100

    change component to class component

--- react-rmdb/src/components/SearchBar/index.js
+++ react-rmdb/src/components/SearchBar/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, {Component} from "react";
 import PropTypes from 'prop-types';
 //Image
 
@@ -7,38 +7,46 @@ import searchIcon from '../../images/search-icon.svg';
 //Styles
 import { Wrapper, Content} from "./SearchBar.styles";
 
-const SearchBar = ({setSearchTerm}) => {
-    const [state, setState] = useState('');
-    const initial = useRef(true);
+class SearchBar extends Component {
+   state = { value: ''};
+   timeout = null;
 
-    
+    componentDidUpdate(_prevProps, prevState){
+        if(this.state.value !== prevState.value) {
+        const { setSearchTerm} = this.props;
 
-    useEffect(()=>{
-        if(initial.current){
-            initial.current = false;
-            return;
-        }
+        this.clearTimeout(this.timeout)
+
+        this.timeout = setTimeout(()=>{
+            const { value } = this.state;
+            setSearchTerm(value)
+        }, 500);
+    }
+}
+
+    render() {
+
+        const {value} = this.state;
+
+        return(
+            <Wrapper>
+                <Content>
+                    <img src={searchIcon} alt='search-icon' />
+                    <input 
+                    type='text' 
+                    placeholder='Search Movie' 
+                    onChange={event => this.setState({ value: event.currentTarget.value})}
+                    value={value}
+                    />
+                </Content>
+            </Wrapper>
+        );
+    };
+    }
         
-        const timer = setTimeout(()=>{
-            setSearchTerm(state)
-        }, 500)
-        return ()=> clearTimeout(timer)
-    },[setSearchTerm, state])
+      
 
-    return(
-        <Wrapper>
-            <Content>
-                <img src={searchIcon} alt='search-icon' />
-                <input 
-                type='text' 
-                placeholder='Search Movie' 
-                onChange={event => setState(event.currentTarget.value)}
-                value={state}
-                />
-            </Content>
-        </Wrapper>
-    );
-};
+   
 
 SearchBar.propTypes = {
     callback: PropTypes.func
